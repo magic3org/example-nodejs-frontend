fcb1fc3720f0e8d05f32a75e088dc9b3eec5da75
naoki hirata <naoki@magic3.org>
2019-02-22 14:29:30 +0900

    不要なファイル削除。

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,77 +30,15 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [
-    '@/assets/style/app.styl',
-
-    '@fortawesome/fontawesome-free/css/all.css',
-  ],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    { src: '@/plugins/vee-validate', ssr: false },
-  ],
+/*  css: [
+    '@/assets/style/app.styl'
+  ],*/
 
   /*
   ** Nuxt.js modules
   */
   modules: [
-    // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
-    '@nuxtjs/vuetify',
-  ],
-  /*
-  ** Axios module configuration
-  */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
-
-  /*
-  ** Customize the progress bar color
-  */
-  vuetify: {
-    //materialIcons: false, // activate if default font style not to be selected
-    iconfont: 'fa',   // component defaut icon font used by system as v-toolbar-side-icon tag. how to use?
-    theme: {
-      primary: '#3f51b5',
-      secondary: '#b0bec5',
-      accent: '#8c9eff',
-      error: '#b71c1c'
-    },
-    /*theme: {
-      "primary": "#1976D2",
-      "secondary": "#424242",
-      "accent": "#82B1FF",
-      "error": "#FF5252",
-      "info": "#2196F3",
-      "success": "#4CAF50",
-      "warning": "#FFC107"
-    }*/
-  },
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-      /*if (process.server && process.browser) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        });
-        config.module.rules.push({
-          test: /\.css$/,
-          loader: ['css-loader', 'stylus-loader'],
-          exclude: /(node_modules)/
-        });
-      } */  
-    }
-  }
+    '@nuxtjs/vuetify'
+  ]
 }
