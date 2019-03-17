0e7eb0c23f8a101bd48a25f028750cbc07fd1f5a
naoki hirata <naoki@magic3.org>
2019-03-18 07:27:46 +0900

    サーバ名設定処理更新。

--- server/api/site.js
+++ server/api/site.js
@@ -4,7 +4,8 @@ const Magic3Env = require(`${appRoot}/server/base/magic3Env`)
 const CF_SITE_NAME = 'site_name' // サイト名
 
 router.get('/info', function (req, res) {
-  const title = Magic3Env.getSiteConfigValue(CF_SITE_NAME) // システム名称
+  let title = Magic3Env.getSiteConfigValue(CF_SITE_NAME) // システム名称
+  if (empty(title)) title = 'サイト名未設定'
   res.json({ ok: true, title: title })
 })
 module.exports = router
